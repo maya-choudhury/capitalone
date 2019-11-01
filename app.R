01c43c3f3b8e549eaec948b28639876e13ef0b03
maya-choudhury <maya.choudhury@duke.edu>
2019-11-01 03:04:05 +0000

    dataframe result working

--- app.R
+++ app.R
@@ -4,8 +4,7 @@ library(readr)
 
 # Load data
 jeopardy <- read_csv("data.csv", col_types = cols(game_id = col_double()))
-
-jeopardy
+library(tidyverse)
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
                 titlePanel("Jeopardy"),
@@ -20,15 +19,20 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                 selected = "100"),
                     
                     # Select date range to be plotted, date or timeframe aired (you can search by a day,  a week, a month)
-                    dateRangeInput("date", strong("Date range"), start = "1985-02-08", end = "2019-07-31",
-                                   min = "1985-02-08", max = "2019-07-31"),
+                    dateRangeInput("date", label =  strong("Date(s) of Episodes"), start = "1984-09-10", end = "2015-03-31",
+                                   min = "1984-09-10", max = "2015-03-31"),
                     
-                    textInput("category", strong("Jeopardy category"))
+                    textInput("category", label = strong("Jeopardy Category")), 
+                    helpText("Note: There are set categories that Jeopardy
+                             uses. To see one try 'comedians' or 'hollywood legends'"),
+                    checkboxInput(inputId = "random", label = "Select Random Game", value=FALSE), 
+                    checkboxInput(inputId = "final", label = "Simulate Final Jeopardy", value=FALSE)
                   ),
                   
                   # Output: Description, lineplot, and reference
                   mainPanel(
-                    plotOutput(outputId = "lineplot"),
+                    DT::dataTableOutput("out"),
+                    #textOutput("final"),
                     tags$a(href = "http://www.jservice.io/", "Source: Jeopardy API")
                   )
                 )
@@ -36,22 +40,70 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
 
 server <- function(input, output) {
   # Subset data
-  set_jeopardy <- reactive({
-    req(input$date)
-    validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
-    validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
-    jeopardy %>%
-      filter(
-        type == input$type,
-        airdate > as.POSIXct(input$date[1]) & airdate < as.POSIXct(input$date[2], 
-                                                                   value == input$dif
-                                                                   # need to filter for category somehow
-        ))
-  })
   
-  output$lineplot <- renderPlot({
-    plot(jeopardy$value, jeopardy$category_id)
+  getInput <- reactive({
+    getSymbols(
+    difficulty = input$dif,
+    type = input$type,
+    date1 = input$date[1],
+    date2 = input$date[2],
+    rand = input$random,
+    final = input$final,
+    auto.assign = FALSE)
+  })
+
+  # set_jeopardy <- reactive({
+  #   if (difficulty>0) {
+  #     jeopardy %>%
+  #       filter(value == difficulty)
+  #   }
+  # })
+  #   if (date1>'1984-09-10' || date2 < '2015-03-31'){
+  #     jeopardy %>%
+  #       filter(airdate <= date2 & airdate>=date1)
+  #   }
+  #   if (type!=""){
+  #     jeopardy %>%
+  #       filter(title == type)
+  #   }
+  #   jeopardy %>%
+  #     slice(1:25)
+  # })
+  # 
+  # finaljeop <- reactive({
+  #   if (final==TRUE) {
+  #     jeopardy %>%
+  #       filter(value==1000) %>%
+  #       sample_n(1)
+  #   }
+  # })
+  # 
+  # output$final <- renderText({
+  #   'There are 30 seconds alloted for final jeopardy' # + finaljeop['question']
+  #   # reactiveTimer(intervalMs = 1000)
+  # })
+  # 
+  output$out <- DT::renderDataTable({
+    difreact <- eventReactive( input$dif, {
+      jeopardy %>%
+        filter(value == input$dif)
+      })
+    datereact <- eventReactive(input$date, {
+      jeopardy %>%
+        filter(airdate <= input$date[2] & airdate>=input$date[1])
+    })
+    typereact <- eventReactive(input$type, {
+      jeopardy %>%
+        filter(title == input$type)
+    })
+  set_jeopardy <- jeopardy %>%
+    filter(!is.na(question), !is.na(answer)) %>% 
+    sample_n(25) %>% 
+    select(question, answer)
+  
+  set_jeopardy
   })
   
 }
+
 shinyApp(ui = ui, server = server)
